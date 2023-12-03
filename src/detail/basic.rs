5e04372defb456b696e06a95c2b5ae6f793fe837
Yun-Jhong Wu <yjwuam@gmail.com>
2023-12-03 17:28:35 -0600

    Make ::new the standard constructor

--- src/detail/basic.rs
+++ src/detail/basic.rs
@@ -1,14 +1,8 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-pub(crate) fn implement_basic(name: &syn::Ident, value_type: &syn::Ident) -> TokenStream {
+pub(crate) fn implement_basic(name: &syn::Ident) -> TokenStream {
     quote! {
-        impl #name {
-            pub fn new(value: impl Into<#value_type>) -> Self {
-                Self(value.into())
-            }
-        }
-
         impl std::fmt::Debug for #name {
             fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
                 f.debug_struct(stringify!(#name))
