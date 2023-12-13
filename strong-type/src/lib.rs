e7738abbacac753110fe90270aea5c84a27e7483
Yun-Jhong Wu <yjwuam@gmail.com>
2023-12-13 09:33:59 -0600

    Replace StrongNumericType with numeric attribute

--- strong-type/src/lib.rs
+++ strong-type/src/lib.rs
@@ -1,5 +1,5 @@
 use std::fmt::Debug;
-pub use strong_type_derive::{StrongNumericType, StrongType};
+pub use strong_type_derive::{StrongNumericType, StrongType}; // TODO[v0.5.0]: Remove StrongNumericType
 
 pub trait StrongType: Debug + PartialEq + PartialOrd + Clone + Default + Send + Sync {
     type UnderlyingType;
