653f18d590e5c353cb61bbf0652662a52acaf1b9
Yun-Jhong Wu <yjwuam@gmail.com>
2023-12-22 15:42:13 -0600

    Bit-shift for integer types

--- strong-type-derive/src/strong_type.rs
+++ strong-type-derive/src/strong_type.rs
@@ -1,8 +1,8 @@
 use crate::detail::{
-    get_attributes, get_type_group, get_type_ident, implement_arithmetic, implement_basic,
-    implement_basic_primitive, implement_basic_string, implement_bool_ops, implement_display,
-    implement_hash, implement_min_max, implement_nan, implement_negate, is_struct_valid,
-    StrongTypeAttributes, UnderlyingTypeGroup,
+    get_type_group, get_type_ident, has_auto_operators, has_custom_display, implement_arithmetic,
+    implement_basic, implement_basic_primitive, implement_basic_string, implement_bit_shift,
+    implement_bool_ops, implement_display, implement_hash, implement_min_max, implement_nan,
+    ,implement_bit_shift, implement_negate, UnderlyingTypeGroup,
 };
 use proc_macro2::TokenStream;
 use quote::quote;
@@ -55,12 +55,18 @@ pub(super) fn expand_strong_type(input: DeriveInput) -> TokenStream {
 
     if has_auto_operators {
         match &group {
-            UnderlyingTypeGroup::Int | UnderlyingTypeGroup::Float => {
+            UnderlyingTypeGroup::Float => {
                 ast.extend(implement_arithmetic(name));
                 ast.extend(implement_negate(name));
             }
+            UnderlyingTypeGroup::Int => {
+                ast.extend(implement_arithmetic(name));
+                ast.extend(implement_negate(name));
+                ast.extend(implement_bit_shift(name));
+            }
             UnderlyingTypeGroup::UInt => {
                 ast.extend(implement_arithmetic(name));
+                ast.extend(implement_bit_shift(name));
             }
             UnderlyingTypeGroup::Bool => {
                 ast.extend(implement_bool_ops(name));
