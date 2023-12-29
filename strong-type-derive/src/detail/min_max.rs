ef116bfa8d3be2df7e3190c21830f002082495db
Yun-Jhong Wu <yjwuam@gmail.com>
2023-12-28 21:18:45 -0600

    Remove min/max in favor of MIN/MAX

--- strong-type-derive/src/detail/min_max.rs
+++ strong-type-derive/src/detail/min_max.rs
@@ -4,13 +4,8 @@ use quote::quote;
 pub(crate) fn implement_min_max(name: &syn::Ident, value_type: &syn::Ident) -> TokenStream {
     quote! {
         impl #name {
-            fn min() -> Self {
-                Self(#value_type::MIN)
-            }
-
-            fn max() -> Self {
-                Self(#value_type::MAX)
-            }
+            const MIN: Self = Self(#value_type::MIN);
+            const MAX: Self = Self(#value_type::MAX);
         }
     }
 }
