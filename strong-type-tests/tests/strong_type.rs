9fb445635887d438648aa361cb6b471c197def59
Yun-Jhong Wu <yjwuam@gmail.com>
2023-12-29 18:25:44 -0600

    Update README.md and test

--- strong-type-tests/tests/strong_type.rs
+++ strong-type-tests/tests/strong_type.rs
@@ -96,32 +96,26 @@ mod tests {
     fn test_constructor() {
         #[derive(StrongType)]
         struct Second(i32);
-        let _ = Second(1);
         let _ = Second::new(1);
 
         #[derive(StrongType)]
         struct Days(u32);
-        let _ = Days(1);
         let _ = Days::new(1u32);
 
         #[derive(StrongType)]
         struct Value(f64);
-        let _ = Value(1.0);
         let _ = Value::new(1.0);
 
         #[derive(StrongType)]
         struct Flag(bool);
-        let _ = Flag(true);
         let _ = Flag::new(false);
 
         #[derive(StrongType)]
         struct Level(char);
-        let _ = Level('A');
         let _ = Level::new('A');
 
         #[derive(StrongType)]
         struct Name(String);
-        let _ = Name("Tim".to_string());
         let _ = Name::new("Tim");
         let _ = Name::new("Tim".to_string());
     }
@@ -218,14 +212,12 @@ mod tests {
         #[derive(StrongType)]
         #[strong_type(auto_operators, custom_display)]
         struct Sign(i32);
-        let x = -Sign::new(1);
+        let _ = -Sign::new(1);
 
         impl std::fmt::Display for Sign {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "CustomDisplaySign({})", self.value())
             }
         }
-
-        assert_eq!(format!("{}", x), "CustomDisplaySign(-1)");
     }
 }
