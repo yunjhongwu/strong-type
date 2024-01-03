62d50ea81ddd9bd7a118e3e6f5afb7e6fb3ed1e1
Yun-Jhong Wu <yjwuam@gmail.com>
2024-01-02 18:48:08 -0600

    Fix visibility of constants

--- strong-type-derive/src/detail/constants.rs
+++ strong-type-derive/src/detail/constants.rs
@@ -4,9 +4,9 @@ use quote::quote;
 pub(crate) fn implement_constants(name: &syn::Ident, value_type: &syn::Ident) -> TokenStream {
     quote! {
         impl #name {
-            const MIN: Self = Self(#value_type::MIN);
-            const MAX: Self = Self(#value_type::MAX);
-            const ZERO: Self = Self(0 as #value_type);
+            pub const MIN: Self = Self(#value_type::MIN);
+            pub const MAX: Self = Self(#value_type::MAX);
+            pub const ZERO: Self = Self(0 as #value_type);
         }
     }
 }
